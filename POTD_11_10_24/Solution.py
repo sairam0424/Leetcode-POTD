e46dfd066e8a4429e06df2dc8dad0513a21c1d7c
sairam0424 <uggesairam0000@gmail.com>
2024-10-11 21:37:18 +0530

    feat(POTD) : added the solution for the leetcode potd added the solution for the leetcode potd for 11_10_24 in python using the sorting

---
+++ POTD_11_10_24/Solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
+
+        target = times[targetFriend]
+
+        times.sort()
+
+        n = len(times)
+
+        chair_time = [0]*(n)
+
+        for time in times:
+
+            for i in range(n):
+
+                if chair_time[i]<=time[0]:
+
+                    chair_time[i] = time[1]
+
+                    if time == target:
+
+                        return i
+
+
+                    break
+
+        return 0
